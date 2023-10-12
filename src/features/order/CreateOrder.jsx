5c245001dce1e641b162e1d9bd05ef129c8ddc2f
zZedx <Kartikajmera890@gmail.com>
2023-10-12 13:00:39 +0530

    geoLocation Added

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 import EmptyCart from "../cart/EmptyCart";
-import store from '../../store'
+import store from "../../store";
 import { clearCart, getTotalCartPrice } from "../cart/cartSlice";
 import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
@@ -19,45 +19,85 @@ const isValidPhone = (str) =>
 function CreateOrder() {
   const navigation = useNavigation();
   const formErrors = useActionData();
-  const dispatch = useDispatch()
-  
-  const username = useSelector(state => state.user.username)
-  const cartTotalPrice = useSelector(getTotalCartPrice)
+  const dispatch = useDispatch();
 
-  const [priority , setPriority] = useState(false)
-  const priorityPrice = priority ? Math.round(cartTotalPrice*20/100) : 0
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === "loading";
+  const cartTotalPrice = useSelector(getTotalCartPrice);
 
-  const totalPrice = cartTotalPrice + priorityPrice 
+  const [priority, setPriority] = useState(false);
+  const priorityPrice = priority ? Math.round((cartTotalPrice * 20) / 100) : 0;
+
+  const totalPrice = cartTotalPrice + priorityPrice;
 
   const isSubmitting = navigation.state === "submitting";
-  const cart = useSelector(state => state.cart.cart);
+  const cart = useSelector((state) => state.cart.cart);
 
-  if(!cart.length) return <EmptyCart/>
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
-      <button onClick={()=> dispatch(fetchAddress())}>Get position</button>
-
       <Form method="POST" action="">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" className="input grow" defaultValue={username} required />
+          <input
+            type="text"
+            name="customer"
+            className="input grow"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
           <div className="grow">
-            <input type="tel" name="phone" className="input w-full" required/>
-            {formErrors?.phone && <p className="text-xs mt-1 ml-3 text-red-500 font-semibold">{formErrors.phone}</p>}
+            <input type="tel" name="phone" className="input w-full" required />
+            {formErrors?.phone && (
+              <p className="ml-3 mt-1 text-xs font-semibold text-red-500">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
-          <div className="grow">
-            <input type="text" name="address" className="input w-full" required/>
+          <div className="relative grow">
+            <input
+              type="text"
+              name="address"
+              className="input w-full"
+              disabled={isLoadingAddress}
+              defaultValue={address}
+              required
+            />
+            {!position.longitude && !position.latitude && (
+              <Button
+                type={"small"}
+                addClass="absolute right-[3px] md:top-[3px]"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+                disabled={isLoadingAddress}
+              >
+                Get position
+              </Button>
+            )}
+            {addressStatus === "error" && (
+              <p className="ml-3 mt-1 text-xs font-semibold text-red-500">
+                {errorAddress}
+              </p>
+            )}
           </div>
         </div>
 
@@ -67,15 +107,28 @@ function CreateOrder() {
             name="priority"
             id="priority"
             className="h-5 w-5 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-1"
-            onChange={()=> setPriority(e => !e)}
+            onChange={() => setPriority((e) => !e)}
           />
-          <label htmlFor="priority" className="font-medium">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button type={"primary"} disabled={isSubmitting}>
-            {isSubmitting ? "Placing Order..." : `ORDER NOW FOR ${formatCurrency(totalPrice)}`}
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.latitude && position.longitude
+                ? `${position.latitude},${position.longitude}`
+                : ""
+            }
+          />
+          <Button type={"primary"} disabled={isSubmitting || isLoadingAddress}>
+            {isSubmitting
+              ? "Placing Order..."
+              : `ORDER NOW FOR ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
@@ -101,7 +154,7 @@ export const action = async ({ request }) => {
 
   const newOrder = await createOrder(order);
 
-  store.dispatch(clearCart())
+  store.dispatch(clearCart());
   return redirect(`/order/${newOrder.id}`);
 };
 
